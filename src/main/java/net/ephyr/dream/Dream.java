370e694b3320aac0a9545fdd19c70da4afd0e833
AirAmetyst <you@example.com>
2023-01-04 21:54:12 +1100

    123

--- src/main/java/net/ephyr/dream/Dream.java
+++ src/main/java/net/ephyr/dream/Dream.java
@@ -1,6 +1,7 @@
 package net.ephyr.dream;
 
 import com.mojang.logging.LogUtils;
+import net.ephyr.dream.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -28,6 +29,7 @@ public class Dream
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
